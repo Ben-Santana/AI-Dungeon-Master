5b83353dfe7f861f568cea5913aebeaf6c465092
Ben-Santana <101908264+Ben-Santana@users.noreply.github.com>
2024-01-28 22:41:36 -0500

    added chat input and messages field

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 function ChatInput() {
   return (
-    <div className="bg-white border-2 p-2 rounded-lg flex">
+    <div className="bg-white border-gray-300 border-2 p-2 rounded-lg flex">
       <input
         className="w-full py-2 px-2 text-gray-800 rounded-lg focus:outline-none"
         type="text"
@@ -22,8 +22,11 @@ export default function GameChat({ adventurers }: { adventurers: Adventurer[] })
 
 
   return (
-    <main className="relative max-w-2xl mx-auto">
-      <div className="sticky top-0 w-full pt-10 px-4">
+    <main className="relative bg-gray-200 max-w-2xl mx-auto h-full p-5 rounded-lg">
+      <div className="w-full h-96 border-gray-300 bg-white border-2 p-2 rounded-lg">
+
+      </div>
+      <div className="w-full">
         <ChatInput />
       </div>
     </main>
