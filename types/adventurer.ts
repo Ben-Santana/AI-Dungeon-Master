f6a4bd269b842f732198b1a90595c82598cd1e88
Victor Santana <victor.santana@cimpress.com>
2024-01-28 20:49:59 -0500

    Added more fields to Adventurer

--- types/adventurer.ts
+++ types/adventurer.ts
@@ -1,11 +1,15 @@
 export interface Adventurer {
     name: String;
     race: String;
+    class: String;
+    level: number;
     stats: Stats;
     hitPoints: HitPoints;
     vigor: Vigor;
     spells: SpellsIndex;
     abilities: Abilities;
+    coins: Coins;
+    inventory: Item[];
 }
 
 export interface Stats {
@@ -37,4 +41,21 @@ export interface SpellsIndex {
 export interface Abilities {
     name: String;
     description: String;
+}
+
+export interface Skills {
+    name: String;
+    description: String;
+}
+
+export interface Coins {
+    gold: number;
+    silver: number;
+    copper: number;
+}
+
+export interface Item {
+    name: String;
+    description: String;
+    uses: number;
 }
