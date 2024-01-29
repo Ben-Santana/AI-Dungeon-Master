0be8c29c07baa1e985ebda16a3e6551acea969e5
Ben-Santana <101908264+Ben-Santana@users.noreply.github.com>
2024-01-28 22:01:42 -0500

    Added ChatInput() in chat.tsx for future player input

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -3,15 +3,29 @@ import { Adventurer } from "../../types/adventurer";
 import Image from "next/image";
 import { useState } from "react";
 
+function ChatInput() {
+  return (
+    <div className="bg-white border-2 p-2 rounded-lg flex">
+      <input
+        className="w-full py-2 px-2 text-gray-800 rounded-lg focus:outline-none"
+        type="text"
+        placeholder="Embark" />
+    </div>
+  )
+}
+
 export default function GameChat({ adventurers }: { adventurers: Adventurer[] }) {
 
   const [chats, setChat] = useState("");
   const [isLoading, setLoading] = useState(false);
-  //const [party, setParty] = useState(["dm", ...adventurers]);
+  const [party, setParty] = useState(["dm", ...adventurers]);
+
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button></button>
+    <main className="relative max-w-2xl mx-auto">
+      <div className="sticky top-0 w-full pt-10 px-4">
+        <ChatInput />
+      </div>
     </main>
   );
 }
