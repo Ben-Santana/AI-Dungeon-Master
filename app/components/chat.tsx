08dcfb518af06bb563f1a2d1c1bb5a6b53de6bab
Ben-Santana <101908264+Ben-Santana@users.noreply.github.com>
2024-01-30 16:14:05 -0500

    Added scrollbar for chat history

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -17,13 +17,14 @@ function ChatInput() {
 //maybe put in types
 interface Message {
   text: string,
-  owner: Adventurer //need to figure out what to do for GPT messages
+  source: string //might need to change String to some DungeonMaster type / Adventurer type if needed
 }
 
-function ChatMessage({ text, owner }: Message) {
+function ChatMessage({ text, source }: Message) {
   return (
     <div>
-
+      <h1>{source}</h1>
+      <p>{text}</p>
     </div>
   )
 }
@@ -36,10 +37,52 @@ export default function GameChat({ adventurers }: { adventurers: Adventurer[] })
   const [messages, setMessages] = useState<Message[]>([]);
 
 
+  //TESTING -----------------------
+
+
+  //setMessages([{ text: "asdasd", source: "asdasd" }])
+
+  //-------------------------------
+
+
   return (
     <main className="relative bg-gray-200 max-w-2xl mx-auto h-full p-5 rounded-lg">
-      <div className="w-full h-96 border-gray-300 bg-white border-2 p-2 rounded-lg">
-
+      <div className="w-full h-96 border-gray-300 bg-white border-2 p-2 rounded-lg overflow-auto overscroll-auto">
+        {messages.map((msg: Message) => (
+          <ChatMessage text={msg.text} source={msg.source} />
+        ))}
+        <h1>Scroll down</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>-</h1>
+        <h1>Hello</h1>
       </div>
       <div className="w-full">
         <ChatInput />
