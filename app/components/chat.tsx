4090493a8d517dd14b986a87c9bd30d3525f14d1
Ben-Santana <101908264+Ben-Santana@users.noreply.github.com>
2024-02-03 21:15:39 -0500

    fixed various visual changes and re-added chat support

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -11,29 +11,29 @@ interface Message {
 
 function ChatMessage({ text, source }: Message) {
   return (
-    <div className="border border-black-300 shadow rounded-md p-4">
-  <div className="animate-pulse flex space-x-4">
-    <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-    <div className="flex-1 space-y-6 py-1">
-      <div className="h-2 bg-slate-700 rounded"></div>
-      <div className="space-y-3">
-        <div className="grid grid-cols-3 gap-4">
-          <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-          <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-        </div>
-        <div className="h-2 bg-slate-700 rounded"></div>
-      </div>
+    <div className="border border-black-300 shadow rounded-md p-4 m-2">
+      <strong>{source}</strong>
+      <p>{text}</p>
     </div>
-  </div>
-</div>
   )
 }
 
 const LoadingMessage = () => {
   return (
-    <div className="rounded-lg bg-gray-100 p-3">
-      <strong>Bot</strong>
-      <p>...</p>
+    <div className="border border-black-300 shadow rounded-md p-4">
+      <div className="animate-pulse flex space-x-4">
+        <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+        <div className="flex-1 space-y-6 py-1">
+          <div className="h-2 bg-slate-700 rounded"></div>
+          <div className="space-y-3">
+            <div className="grid grid-cols-3 gap-4">
+              <div className="h-2 bg-slate-700 rounded col-span-2"></div>
+              <div className="h-2 bg-slate-700 rounded col-span-1"></div>
+            </div>
+            <div className="h-2 bg-slate-700 rounded"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
@@ -101,35 +101,35 @@ export default function GameChat({ adventurers }: { adventurers: Adventurer[] })
   return (
     <main className="h-full p-5 relative bg-gray-200 rounded-lg">
       <div className="h-full grid grid-cols-1 grid-rows-12 grid-flow-col gap-3">
-      <div className="row-span-11 border-gray-300 bg-white border-2 rounded-lg overflow-auto overscroll-auto scrollbar-thumb:!rounded">
-        {messages.map((msg: Message) =>
-        <ChatMessage text={msg.text} source={msg.source} key={msg.key} />
-        )}
+        <div className="row-span-11 border-gray-300 bg-white border-2 rounded-lg overflow-auto overscroll-auto scrollbar-thumb:!rounded">
+          {messages.map((msg: Message) =>
+            <ChatMessage text={msg.text} source={msg.source} key={msg.key} />
+          )}
 
-            {loading
-              ? <LoadingMessage/>
-              : <></>
-            }
+          {loading
+            ? <LoadingMessage />
+            : <></>
+          }
 
-      </div>
-      <div className="row-span-1">
-        <div className="h-full bg-white border-gray-300 border-2 p-2 rounded-lg flex">
-          <form className="w-full" onSubmit={submitText}>
-            <input
-              className="h-full w-11/12 py-2 px-2 text-gray-800 rounded-lg focus:outline-none"
-              type="text"
-              placeholder="Embark"
-              value={input}
-              onChange={handleInputChange}
-              disabled={loading}
-            />
-            <label>
-            <input className="h-19 w-1/12 bg-gray-800 text-center hover:cursor-pointer bg-[url('../../public/svg/d20-dice.svg')]" type="submit" value="">
-            </input>
-            </label>
-          </form>
         </div>
-      </div>
+        <div className="row-span-1">
+          <div className="h-full bg-white border-gray-300 border-2 p-1 rounded-lg flex items-center">
+            <form className="w-full h-full flex items-center" onSubmit={submitText}>
+
+              <input
+                className="h-full w-11/12 text-gray-900 indent-3 rounded-lg focus:outline-none p-0 justify-self-center"
+                type="text"
+                placeholder="Embark"
+                value={input}
+                onChange={handleInputChange}
+                disabled={loading}
+              />
+
+              <input className="h-full w-1/12 text-center hover:cursor-pointer custom_submit-bg" type="submit" value="" />
+
+            </form>
+          </div>
+        </div>
       </div>
     </main >
   );
